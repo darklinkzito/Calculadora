38c7479a056d62f6e70789b51871b7e0c3fecc20
darklinkzito <andre_luiz_7777@hotmail.com>
2022-03-20 10:40:31 -0300

    css corrigido

--- scripts.js
+++ scripts.js
@@ -17,6 +17,6 @@ function calcular() {
     if (resultado) {
         document.getElementById("resultado").innerHTML = eval(resultado);
     }else{
-        document.getElementById("resultado").innerHTML = "invalido";
+        document.getElementById("resultado").innerHTML = "Error";
     }
 }
