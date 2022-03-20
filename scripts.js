066bf04f6704b27493727ceaf344fc1e637cb526
darklinkzito <andre_luiz_7777@hotmail.com>
2022-03-20 10:09:20 -0300

    .

--- scripts.js
+++ scripts.js
@@ -16,5 +16,7 @@ function calcular() {
     var resultado = document.getElementById("resultado").innerHTML;
     if (resultado) {
         document.getElementById("resultado").innerHTML = eval(resultado);
+    }else{
+        document.getElementById("resultado").innerHTML = "invalido";
     }
 }
