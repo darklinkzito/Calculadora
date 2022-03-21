51414836a8859182c855dff3bbea40255d3593e3
darklinkzito <andre_luiz_7777@hotmail.com>
2022-03-21 19:25:26 -0300

    Implementacoes extras na calculadora

--- scripts.js
+++ scripts.js
@@ -1,13 +1,34 @@
+let calculado = false;
+
 function insert(num) {
+    if (calculado == false) {
+        let numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + num;
+    } else {
+        document.getElementById('resultado').innerHTML = "";
+        let numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + num;
+        calculado = false;
+    }
+}
+
+function insert_operator(num) {
     let numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
+    calculado = false;
+    if (calculado == true) {
+        let numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + num;
+    }
 }
 
 function clean() {
     document.getElementById('resultado').innerHTML = "";
+    calculado = false;
 }
 
 function back() {
+    calculado = false;
     var resultado = document.getElementById("resultado").innerHTML;
     document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length - 1);
 }
@@ -16,7 +37,9 @@ function calcular() {
     var resultado = document.getElementById("resultado").innerHTML;
     if (resultado) {
         document.getElementById("resultado").innerHTML = eval(resultado);
-    }else{
+        calculado = true;
+    } else {
         document.getElementById("resultado").innerHTML = "Error";
+        calculado = false;
     }
 }
