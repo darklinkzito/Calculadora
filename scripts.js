759a29ea07322d54193cec74e689e367cb9c2fda
darklinkzito <andre_luiz_7777@hotmail.com>
2022-03-22 22:24:55 -0300

    Calculadora Finalizada

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 let calculado = false;
 let numero;
 
+
+//função para inserir número na calculadora .
+//if para confirmar se o valor mostrado na tela é resultado de um calculo, se sim irá zerar tela ao digitar novo número.
+
 function insert(num) {
     if (calculado == false) {
         numero = document.getElementById('resultado').innerHTML;
@@ -13,24 +17,31 @@ function insert(num) {
     }
 }
 
+//função para inserir um operador.
+
 function insert_operator(num) {
-    tecla = num;
-    numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
-    calculado = false;
-    if (numero.substring(numero.length-1, numero.length) == num) {
-        var resultado = document.getElementById("resultado").innerHTML;
-        document.getElementById("resultado").innerHTML = num.replace(num, tecla);
-    } else {
-        var resultado = document.getElementById("resultado").innerHTML;
-        document.getElementById("resultado").innerHTML = numero + num;
+    var resultado = document.getElementById('resultado').innerHTML;
+    var ultimo_carac = resultado.charAt(resultado.length - 1);
+    var cond = !isNaN(ultimo_carac);
+    if (cond == true) {
+        resultado = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = resultado + num;
+        cond = false;
     }
+    else {
+        document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1) + num;
+    }
+    calculado = false;
 }
+
+//função para limpar tela.
 
 function clean() {
     document.getElementById('resultado').innerHTML = "";
     calculado = false;
 }
+
+//função para apagar ultimo número caracter da tela.
 
 function back() {
     calculado = false;
@@ -38,13 +49,20 @@ function back() {
     document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
+//função para calcular resultado da conta na tela.
+
 function calcular() {
     var resultado = document.getElementById("resultado").innerHTML;
-    if (resultado) {
+    var ultimo_carac = resultado.charAt(resultado.length - 1);
+    var eNum = !isNaN(ultimo_carac);
+
+//se ultimo caracter for um número fazer conta senão não é um calculo.
+
+    if (eNum == true) {
         document.getElementById("resultado").innerHTML = eval(resultado);
         calculado = true;
     } else {
-        document.getElementById("resultado").innerHTML = "Error";
+        alert("expressão digitada não é um calculo válido!");
         calculado = false;
     }
 }
